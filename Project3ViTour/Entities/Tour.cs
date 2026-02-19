cb4841ff15110ad2c03f18a4c88ce8ec8da29fc3
UMUT CAN YAVRU <umutcanyavru@gmail.com>
2026-02-19 23:07:03 +0300

    Remove Mapbox token

---
+++ Project3ViTour/Entities/Tour.cs
@@ -0,0 +1,15 @@
+﻿namespace Project3ViTour.Entities
+{
+    public class Tour
+    {
+        public string TourId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CoverImageUrl { get; set; }
+        public string Badge { get; set; }
+        public int DayCount { get; set; }
+        public int CapCity { get; set; }
+        public decimal Price { get; set; }
+        public bool IsStatus { get; set; }
+    }
+}

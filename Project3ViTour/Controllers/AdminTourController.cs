4a40bb04b8bd7ff887eb970d80d07db297833da5
UMUT CAN YAVRU <umutcanyavru@gmail.com>
2026-02-24 21:09:33 +0300

    tour guncelleme

---
+++ Project3ViTour/Controllers/AdminTourController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project3ViTour.Dtos.TourDtos;
+using Project3ViTour.Services.TourService;
+
+namespace Project3ViTour.Controllers
+{
+    public class AdminTourController : Controller
+    {
+        private readonly ITourService _tourService;
+
+        public AdminTourController(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+
+        public async Task<IActionResult> TourList()
+        {
+            var values = await _tourService.GetAllToursAsync();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateTour()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTour(CreateTourDto createDtoTour)
+        {
+            await _tourService.CreateTourAsync(createDtoTour);
+            return RedirectToAction("TourList");
+        }
+        
+    }
+}
--- Project3ViTour/Entities/Tour.cs
+++ Project3ViTour/Entities/Tour.cs
@@ -1,14 +1,19 @@
-﻿namespace Project3ViTour.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Project3ViTour.Entities
 {
     public class Tour
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string TourId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string CoverImageUrl { get; set; }
         public string Badge { get; set; }
         public int DayCount { get; set; }
-        public int CapCity { get; set; }
+        public int CapaCity { get; set; }
         public decimal Price { get; set; }
         public bool IsStatus { get; set; }
     }
